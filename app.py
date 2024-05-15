4a4db44f438c89db0882e91401e51e5a7824bf57
Rich Chiodo <rchiodo@users.noreply.github.com>
2024-05-15 16:28:57 +0000

    Update to Sprint 5

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 ###############################################################################
-## Sprint 3: Database Integration
-## Feature 1: Add Voice
-## User Story 1: Add Voice to Text
-###############################################################################
+## Sprint 5: Advanced AI Recommendations
+## Feature 1: Get Gen AI Recommendation 
+## User Story 3: Refresh Recommendations 
+############################################################################
 import os
+import json
 from flask import Flask, render_template, request, redirect, url_for, g
 from database import db, Todo
 from recommendation_engine import RecommendationEngine
+
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))   # Get the directory of the this file
@@ -25,9 +27,10 @@ def load_data_to_g():
     g.todos = todos 
     g.todo = None
 
+
 @app.route("/")
 def index():
-    return render_template("index.html", todo_list=g.todos)
+    return render_template("index.html")
 
 @app.route("/add", methods=["POST"])
 def add_todo():
@@ -35,6 +38,7 @@ def add_todo():
     todo = Todo(
         name=request.form["todo"],
     )
+
     # Add the new ToDo to the list
     db.session.add(todo)
     db.session.commit()
@@ -49,14 +53,43 @@ def remove_todo(id):
     db.session.commit()
     return redirect(url_for('index'))
 
-# show AI recommendations
-@app.route("/recommend/<int:id>", methods=["GET"])
-async def recommend(id):
+# Show AI recommendations
+@app.route('/recommend/<int:id>', methods=['GET'])
+@app.route('/recommend/<int:id>/<refresh>', methods=['GET'])
+async def recommend(id, refresh=False):
     recommendation_engine = RecommendationEngine()
     g.todo = db.session.query(Todo).filter_by(id=id).first()
-    g.todo.recommendations = await recommendation_engine.get_recommendations(g.todo.name)
-        
-    return render_template("index.html")
+
+    if g.todo and not refresh:
+        try:
+            #attempt to load any saved recommendation from the DB
+            if g.todo.recommendations_json is not None:
+                g.todo.recommendations = json.loads(g.todo.recommendations_json)
+                return render_template('index.html')
+        except ValueError as e:
+            print("Error:", e)
+
+    previous_links_str = None
+    if refresh:
+        g.todo.recommendations = json.loads(g.todo.recommendations_json)
+        # Extract links
+        links = [item["link"] for item in g.todo.recommendations]
+        # Convert list of links to a single string
+        previous_links_str = ", ".join(links)
+
+    g.todo.recommendations = await recommendation_engine.get_recommendations(g.todo.name, previous_links_str)
+    
+    # Save the recommendations to the database
+    try:
+        g.todo.recommendations_json = json.dumps(g.todo.recommendations)
+        db.session.add(g.todo)
+        db.session.commit()
+    except Exception as e:
+        print(f"Error adding and committing todo: {e}")
+        return
+
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String, Boolean, func
+from sqlalchemy import Integer, String, Boolean, JSON, func
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
@@ -10,7 +10,11 @@ db = SQLAlchemy(model_class=Base)
 class Todo(db.Model):
     id = db.Column(Integer, primary_key=True)
     name = db.Column(String(100), nullable=False)
-    recommendations_json = db.Column(db.JSON)    
-    
+    recommendations = []
+    recommendations_json = db.Column(db.JSON)
+
     def __str__(self):
         return self.name
+    
+    
+    
