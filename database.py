aaccd180c05510f5299fc55d18dfdb528a673eab
Rich Chiodo <rchiodo@users.noreply.github.com>
2024-05-15 18:19:40 +0000

    Finish sprint 7

--- database.py
+++ database.py
@@ -12,6 +12,33 @@ class Todo(db.Model):
     name = db.Column(String(100), nullable=False)
     recommendations = []
     recommendations_json = db.Column(db.JSON)
+    notes = db.Column(String(100))
+    priority = db.Column(Integer, default=0)
+    completed = db.Column(Boolean, default=False)
+    due_date = db.Column(String(50))
+
+    def priority_str(self):
+        """
+        Returns the priority as a string.
+        """
+        str = "Not Set"
+        if self.priority == 1:
+            str = "High"
+        elif self.priority == 2:
+            str = "Medium"
+        elif self.priority == 3:
+            str = "Low"
+
+        return str
+
+    def completed_str(self):
+        """
+        Returns the completed status as a string.
+        """
+        if self.completed:
+            return "Yes"
+        else:    
+            return "No"
 
     def __str__(self):
         return self.name
